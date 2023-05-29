71d5050293976326b0b56e4d47c98710a3fa57ed
Wesley Peters <wesleypeters@gmail.com>
2023-05-29 14:00:57 -0600

    adds user functionality to Task Lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    buildNewTask(e.target.new_task_description.value)
+    form.reset()
+  })
+
 });
+
+function buildNewTask(newTask){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "x"
+  li.textContent = `${newTask} `
+  li.appendChild(btn)
+  document.getElementById('tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
